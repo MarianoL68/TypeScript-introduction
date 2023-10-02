28add345e1975a88945f573bd2dde98a53fcac52
Mariano Loza <marianoloza68@gmail.com>
2023-10-02 18:40:43 -0300

    classes finished

--- 07-classes.ts
+++ 07-classes.ts
@@ -1,12 +1,29 @@
-class Avenger {
-    readonly name: string
-    private powerScore: number 
-    private readonly wonBattles: number = 0
-    protected age: number = 0
+interface Avenger {
+    name: string
+    powerScore: number
+    wonBatlles: number
+    age: number
+    // battle: (enemy: Avenger, win: boolean) => void
+}
+
+class Avenger implements Avenger {
+    // readonly name: string
+    // private powerScore: number 
+    // private readonly wonBattles: number = 0
+    // protected age: number = 0
 
     constructor(name: string, powerScore: number ) {
         this.name = name
         this.powerScore = powerScore
+    }
+
+    battle (enemy, win) {
+        if(win) {
+            this.wonBatlles++
+            this.powerScore += 5
+        } else {
+            this.powerScore -= 5
+        }
     }
 
     get fullName() {
